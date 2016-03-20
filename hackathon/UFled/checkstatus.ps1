c7b1d82784116583a9008dd1d4fc2a18e99eb157
WimCaudenbergh <wim.caudenbergh@gmail.com>
2016-03-20 20:47:26 +0100

    Got rid of hardcoded entries, retry on timeout, extra logging, extra functions, ...

--- hackathon/UFled/checkstatus.ps1
+++ hackathon/UFled/checkstatus.ps1
@@ -1,12 +1,15 @@
 Import-Module ".\changeDevices.psm1"
 
 $xml = New-Object System.Xml.XmlDocument
-$web = New-Object Net.WebClient
+# $web = New-Object Net.WebClient
 $devicesXML = Get-DevicesXML 
 $DevicesLoaded = 0
 Set-Variable -name DevicesLoaded -scope Script
 $DeviceRefreshTime = 5
 $NextRefresh = $DeviceRefreshTime
+$newline = [Environment]::NewLine
+$deviceNode = $null
+$state = "Unchecked"
 
 Function Start-Monitoring
 {
@@ -14,25 +17,85 @@ Function Start-Monitoring
     {
 		
         Update-DeviceXML
-    
-        $xml.Load("https://monitor.uf.be/api/getobjectstatus.htm?id=25272&name=status&show=text&username=ufadmin&passhash=560266657")
-		
-		$status = $xml.prtg.result 
-    
+
         $NextRefresh = $DeviceRefreshTime - $DevicesLoaded
-		write-host "Sensor status: $status             Next device refresh in: $NextRefresh"
+        
+        # //TODO Loop all devices from XML file
 
-		# if ($status -like '*up*') { 
-		# 	$web.DownloadString("http://192.168.76.105/state/green")
-		# }elseif($status -like '*down*'){
-		# 	$web.DownloadString("http://192.168.76.105/state/red")
-		# }else{
-		# 	$web.DownloadString("http://192.168.76.105/state/blue")
-		# }
-
-		
+        Update-DeviceState 'ESP Rack 2'
+	
         Start-Sleep -s 1
     }
+}
+
+Function Update-DeviceState($name)
+{
+
+    if ($deviceNode -eq $null) {
+        $Script:deviceNode = $Script:devicesXML.devices.device | where {$_.name -eq $name}
+    }
+        
+    $PRTGurl= "https://monitor.uf.be/api/getobjectstatus.htm?id=" + $deviceNode.PRTGidToCheck + "&name=status&show=text&username=ufadmin&passhash=560266657"
+    $xml.Load($PRTGurl)
+    $Script:state = $xml.prtg.result 
+
+    #cut of the "(simulated error)" text if exists    
+    if ($state -Match "simulated") 
+    { 
+        $state = $state.Substring(0, $state.IndexOf(' '))
+    }    
+
+    $state = $state.Trim()
+    
+    if ($deviceNode.currentState -ne $state) {
+        $s = $deviceNode.currentState
+        
+        write-host $newline"Change state of " -nonewline; 
+        write-host "$name " -nonewline -foregroundcolor yellow; 
+        write-host "from " -nonewline;
+        write-host "$s " -nonewline -foregroundcolor red;
+        write-host "to " -nonewline;
+        write-host "$state" -foregroundcolor red
+        
+        $deviceNode.currentState = $state
+        $deviceNode
+        Update-LedState $deviceNode.ip $state
+    }
+
+    # keep trying if timedout last time
+    if ($deviceNode.LEDupToDate -eq "No") {
+        Update-LedState $deviceNode.ip $state
+    }
+
+
+}
+
+Function Update-LedState($ip, $state)
+{
+
+    $ESPurl = "http://$ip/state/$state"
+    # $ESPurl = "http://google.be"
+    write-host "Set LED-state to " -nonewline; 
+    write-host "$state " -nonewline -foregroundcolor red; 
+    write-host "on "  -nonewline; 
+    write-host "$ip" -nonewline -foregroundcolor yellow; 
+    write-host "       ==>  $ESPurl" -foregroundcolor green
+
+    #try invoking the ESP url that switches the LEDs. Do nothing if the page times out. 
+    try {
+        Invoke-WebRequest $ESPurl -TimeoutSec 3  > $null 
+
+    }catch{
+        write-host $newline "Timed out while requesting $ESPurl" $newline
+        $timeout=$true
+    }
+
+    if (!$timeout) {
+        write-host "connected to webserver"
+        $Script:deviceNode.LEDupToDate = "Yes"
+        $timeout=$false
+    }
+
 }
 
 Function Update-DeviceXML
@@ -40,14 +103,13 @@ Function Update-DeviceXML
     # update devices if not updated a minute ago
 
     if ($DevicesLoaded -ge $DeviceRefreshTime) {
-        $devicesXML = Get-DevicesXML   
-        write-host "current devices loaded:"
-        $devicesXML.devices.device
-        
+        $Script:devicesXML = Get-DevicesXML   
+      
         $Script:DevicesLoaded = 0 
     }else {
         $Script:DevicesLoaded += 1
     }    
 }
+
 
 Start-Monitoring
