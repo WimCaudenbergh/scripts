f436e953a026373e3c821a03adeaabf5899b9305
WimCaudenbergh <wim.caudenbergh@gmail.com>
2016-08-05 00:13:40 +0200

    more functions! Wrapping up step1. AAD Sync.

--- work/AD/CreateADUser.ps1
+++ work/AD/CreateADUser.ps1
@@ -10,33 +10,42 @@
 ########################################################
 
 function checkPassword{
-    if ($Password -ne $Password2) {
+    if ($global:Password -ne $global:Password2) {
         Write-Host "`n ---------Retype the password---------"
-        $script:Password = Read-Host ' Password:'
-        $script:Password2 = Read-Host ' Retype Password:'
+        $global:Password = Read-Host ' Password:'
+        $global:Password2 = Read-Host ' Retype Password:'
         checkpassword
     }else{
+
         Write-Host "`n Password set"
     }
 }
 
+function checkIfUsernameExists($username){
+    $userexists=$null
+    Try { 
+        $userexists = Get-ADUser -Identity $global:username 
+    } Catch {}
+    
+    if (userexists) {
+        $global:username = Read-Host "`n Username $global:username exists, fill in a new username (without @uf.be)"
+    }
+}
+
+#TODO: generate password if left empty
 function generateUsername($first, $last){
     $first = $first.Substring(0,1)
     $last = $last.Substring(0,1)
-    $script:username = "$first$last"
+    $global:username = "$first$last"
 
     #TODO check if username exists
-    # userexists=$null
-    # Try { userexists = Get-ADUser -Identity $script:username } Catch {}
-    # if (userexists) {
-    #     $script:username = Read-Host "`n Username $script:username exists, fill in a new username (without @uf.be)"
-    # }
+    #checkIfUsernameExists $global:username
 
     $title = " "
-    $message = " The username $script:username@uf.be will be used. Continue?"
+    $message = " The username $global:username@uf.be will be used. Continue?"
 
     $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
-        " Use $script:username@uf.be as the username."
+        " Use $global:username@uf.be as the username."
 
     $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
         " Choose a new username."
@@ -47,8 +56,8 @@ function generateUsername($first, $last){
 
     switch ($result)
         {
-            0 {" Username set to $script:username@uf.be"}
-            1 {$script:username = Read-Host " Fill in a new username (without @uf.be)"}
+            0 {" Username set to $global:username@uf.be"}
+            1 {$global:username = Read-Host " Fill in a new username (without @uf.be)"}
         }
 
 }
@@ -56,16 +65,16 @@ function generateUsername($first, $last){
 function generateAddresses($first, $last){
 
     $last = $last -replace '\s',''
-    $script:primarymail = $firstname+"."+$lastname+"@Userfull.be"
-    $script:proxyaddress = $script:username+"@uf.be"
+    $global:primarymail = $firstname+"."+$lastname+"@Userfull.be"
+    $global:proxyaddress = $global:username+"@uf.be"
 
 }
 
 function checkMobile($nr){
 
     if ($nr -NotMatch '^\+\d{2}\s\d{3}\s\d{2}\s\d{2}\s\d{2}$') {
-        $script:Mobile = Read-Host "`n incorrect format (example: +32 123 45 67 89)"
-        checkMobile $script:Mobile
+        $global:Mobile = Read-Host "`n incorrect format (example: +32 123 45 67 89)"
+        checkMobile $global:Mobile
     }
 
 }
@@ -109,15 +118,15 @@ function askRole{
 
     switch ($result)
         {
-            0 {$script:userOU = "OU=APP,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            1 {$script:userOU = "OU=HR,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            2 {$script:userOU = "OU=Management,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            3 {$script:userOU = "OU=MSP,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            4 {$script:userOU = "OU=OI,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            5 {$script:userOU = "OU=PLT,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            6 {$script:userOU = "OU=SOL,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            7 {$script:userOU = "OU=Interns,OU=Users,OU=Userfull,DC=UF,DC=be"}
-            8 {$script:userOU = "OU=External,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            0 {$global:userOU = "OU=APP,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            1 {$global:userOU = "OU=HR,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            2 {$global:userOU = "OU=Management,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            3 {$global:userOU = "OU=MSP,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            4 {$global:userOU = "OU=OI,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            5 {$global:userOU = "OU=PLT,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            6 {$global:userOU = "OU=SOL,OU=Employees,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            7 {$global:userOU = "OU=Interns,OU=Users,OU=Userfull,DC=UF,DC=be"}
+            8 {$global:userOU = "OU=External,OU=Users,OU=Userfull,DC=UF,DC=be"}
 
         }
 
@@ -139,8 +148,8 @@ function setDeveloper {
 
     switch ($result)
         {
-            0 {$script:devgroup = $True}
-            1 {$script:devgroup = $False}
+            0 {$global:devgroup = $True}
+            1 {$global:devgroup = $False}
         }
 }
 
@@ -160,8 +169,8 @@ function setTechsupport {
 
     switch ($result)
         {
-            0 {$script:uftechgroup = $True}
-            1 {$script:uftechgroup = $False}
+            0 {$global:uftechgroup = $True}
+            1 {$global:uftechgroup = $False}
         }
 }
 
@@ -181,111 +190,164 @@ function setFAP {
 
     switch ($result)
         {
-            0 {$script:fapgroup = $True}
-            1 {$script:fapgroup = $False}
+            0 {$global:fapgroup = $True}
+            1 {$global:fapgroup = $False}
         }
 }
 
-########################################################
-#ask values
-########################################################
 
+function syncWithO365 {
+    $title = " ------------------ `n O365 Sync"
+    $message = " Sync the on premise Active Directory with Office 365?"
 
-$FirstName = Read-Host "`n First name"
-$LastName = Read-Host "`n Last name"
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+        " Sync now"
 
-generateUsername $FirstName $LastName
-generateAddresses $FirstName $LastName
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+        " Don't sync"
 
-askRole
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 
-$Password = Read-Host "`n Password"
-$Password2 = Read-Host " Retype Password"
+    $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
 
-checkPassword
-
-$Mobile = Read-Host "`n Mobile number (+32 123 45 67 89)"
-
-checkMobile $Mobile
-
-$defaultTwitter = 'https://twitter.com/Userfull_ICT'
-$Twitter = Read-Host "`n Twitter url (leave blank for $($defaultTwitter))"
-$Twitter = ($defaultTwitter,$Twitter)[[bool]$Twitter]
-
-
-
-$defaultLinkedIn = 'https://www.linkedin.com/company/userfull'
-$LinkedIn = Read-Host "`n LinkedIn url (leave blank for $($defaultLinkedIn))"
-$LinkedIn = ($defaultLinkedIn,$LinkedIn)[[bool]$LinkedIn]
-
-
-setDeveloper
-
-setTechsupport
-
-setFAP
-
-
-########################################################
-#output
-########################################################
-
-
-Write-Host "`n `n -----------------------------------"
-
-Write-Host " First name:`t" $FirstName
-Write-Host " Last name:`t" $LastName
-Write-Host " username:`t username@uf.be"
-Write-Host " User OU:`t" $userOU
-Write-Host " Password:`t" $Password
-Write-Host " Twitter:`t" $Twitter
-Write-Host " Mobile nr:`t" $Mobile
-Write-Host " LinkedIn:`t" $LinkedIn
-Write-Host " Developer:`t" $devgroup
-Write-Host " Techsupport:`t" $uftechgroup
-Write-Host " FAP:`t`t" $fapgroup
-Write-Host "`n emailaddr:`t" $primarymail
-Write-Host " ProxyAddr1:`t SMTP:$primarymail"
-Write-Host " ProxyAddr2:`t smtp:$proxyaddress"
-Write-Host "`n Adding to default groups: `n `t - distr_medewerkers" 
-
-Write-Host "----------------------------------- `n"
-
-$correct = Read-Host "`n An account with the previous attributes will be created. Continue?"
-while("y","n" -notcontains $correct)
-{
-    $correct = Read-Host " Y or N"
-}
-
-if ($correct -eq "y") {
-    Write-Host 'continue'
-}else{
-    Write-Host "`n exiting"
-    exit
+    switch ($result)
+        {
+            0 {Start-Process "C:\Program Files\Microsoft Azure AD Sync\Bin\DirectorySyncClientCmd.exe" -Wait}
+            1 {"Skipping sync with O365"}
+        }
+    Write-Host "`n ------------------"
 }
 
 
+function askValues{
+    $global:FirstName = Read-Host "`n First name"
+    $global:LastName = Read-Host "`n Last name"
+
+    generateUsername $global:FirstName $global:LastName
+    generateAddresses $global:FirstName $global:LastName
+
+    askRole
+
+    $global:Password = Read-Host "`n Password"
+    $global:Password2 = Read-Host " Retype Password"
+
+    checkPassword
+
+    $global:Mobile = Read-Host "`n Mobile number (+32 123 45 67 89)"
+
+    checkMobile $global:Mobile
+
+    $defaultTwitter = 'https://twitter.com/Userfull_ICT'
+    $Twitter = Read-Host "`n Twitter url (leave blank for $($defaultTwitter))"
+    $global:Twitter = ($defaultTwitter,$Twitter)[[bool]$Twitter]
+
+
+
+    $defaultLinkedIn = 'https://www.linkedin.com/company/userfull'
+    $LinkedIn = Read-Host "`n LinkedIn url (leave blank for $($defaultLinkedIn))"
+    $global:LinkedIn = ($defaultLinkedIn,$LinkedIn)[[bool]$LinkedIn]
+
+
+    setDeveloper
+
+    setTechsupport
+
+    setFAP
+}
+
+function showValues{
+
+    Write-Host "`n `n -----------------------------------"
+
+    Write-Host " First name:`t" $global:FirstName
+    Write-Host " Last name:`t" $global:LastName
+    Write-Host " username:`t" $global:username"@uf.be"
+    Write-Host " User OU:`t" $global:userOU
+    Write-Host " Password:`t" $global:Password
+    Write-Host " Twitter:`t" $global:Twitter
+    Write-Host " Mobile nr:`t" $global:Mobile
+    Write-Host " LinkedIn:`t" $global:LinkedIn
+    Write-Host " Developer:`t" $global:devgroup
+    Write-Host " Techsupport:`t" $global:uftechgroup
+    Write-Host " FAP:`t`t" $global:fapgroup
+    Write-Host "`n emailaddr:`t" $global:primarymail
+    Write-Host " ProxyAddr1:`t SMTP:$global:primarymail"
+    Write-Host " ProxyAddr2:`t smtp:$global:proxyaddress"
+    Write-Host "`n Adding to default groups: `n `t - distr_medewerkers" 
+
+    Write-Host "----------------------------------- `n"
+}
+
+function step1{
+    askValues
+    showvalues
+    askForAccountCreation
+}
+
+function askForAccountCreation{
+    $title = " create user"
+    $message = " An account with the listed attributes will be created. Continue?"
+
+    $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes", `
+        " Create user."
+
+    $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", `
+        " Start over"
+
+    $stop = New-Object System.Management.Automation.Host.ChoiceDescription "&Stop", `
+        " Stop the script"
+
+    $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no, $stop)
+
+    $result = $host.ui.PromptForChoice($title, $message, $options, 0) 
+
+    switch ($result)
+        {
+            0 {createUser}
+            1 {step1}
+            2 {exit}
+        }
+}
+
+#TODO: create user
+function createUser{
+
+    Write-Host "creating AD user"
+    #correct dial-in settings
+    #Get-ADUser username| Set-ADUser -replace @{msnpallowdialin=$true}
+
+}
+########################################################
+#Step1: ask values, show them and create user
+########################################################
+
+step1
+
+
 
 ########################################################
-#TODO: Create the user
+#TODO: Step2: Sync with Office365
 ########################################################
 
-#correct dial-in settings
-#Get-ADUser username| Set-ADUser -replace @{msnpallowdialin=$true}
-
-
-########################################################
-#TODO: Sync with Office365
-########################################################
+#Azure AD sync
+#syncWithO365
 
 ########################################################
-#TODO: Office365 settings (licenses, groups, ...)
+#TODO: Step3: Office365 settings
 ########################################################
 
-########################################################
-#TODO: Exchange online settings
-########################################################
+    ########################################################
+    #TODO: Office365 settings (licenses, groups, ...)
+    ########################################################
+
+    ########################################################
+    #TODO: Exchange online settings
+    ########################################################
+
+    ########################################################
+    #TODO: SFB settings
+    ########################################################
 
 ########################################################
-#TODO: SFB settings
+#TODO: Logging
 ########################################################
